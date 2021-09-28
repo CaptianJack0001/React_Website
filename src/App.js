a8e9fb01d24542e16d917031b63488ba6e6407d5
Sai <saishivamkaushal0001@gmail.com>
2021-09-29 00:51:22 +0530

    save

--- src/App.js
+++ src/App.js
@@ -2,14 +2,47 @@ import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
+import React, { useState } from 'react'
+import Alert from "./components/Alert";
+
 
 function App() {
-  return (
+  const [mode, setmode] = useState('light');//check whether dark mode enabled or not
+  
+const [alert, setAlert] = useState(null);
+const showAlert=(message,type)=>{
+  setAlert({
+    msg:message,
+    type:type
+  })
+  setTimeout(()=>{
+setAlert(null);
+  }
+,1000 );
+}
+  const toggleMode=()=>{
+    if(mode==='light')
+    {
+    setmode('dark');
+    document.body.style.backgroundColor='#042743';
+    showAlert("Dark mode has been enabled","success");
+    }
+    else 
+    {
+      document.body.style.backgroundColor='white';
+    setmode('light');
+    showAlert("Light mode has been enabled","success");
+    }
+  }
+
+  return (   
     <>
-      <Navbar title="Myfirstapp" about="AboutUS" />
-      <div className="container">
-        <Textform  heading ="convert" />
+      <Navbar title="Navbar" about="about" mode={mode} toggleMode={toggleMode}/>
+           <Alert alert={alert}/>   
+      <div className="container my-3"  >
+        <Textform    showAlert={showAlert} heading ="Enter the text here to convert..."  mode ={mode} toggleMode={toggleMode} />
         {/* <About/> */}
+
       </div>
     </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"; //import function for state
+import PropTypes from 'prop-types'
 
 export default function Textform(props) {  //prop means passsing propetyy
   
@@ -6,11 +7,13 @@ export default function Textform(props) {  //prop means passsing propetyy
     console.log("uppercase was clicked", + text);   
     let newtext=text.toUpperCase();
     setText(newtext);
+    props.showAlert("Converted to upper case","success");
   }
   const handletoClick =()=>{   //text inside textarea
     console.log("uppercase was clicked", + text);   
     let newtext=text.toLowerCase();
     setText(newtext);
+    props.showAlert("Converted to lower case","failure");
   }
   const handleOnChange=(event)=>{
   console.log("on change");
@@ -18,28 +21,37 @@ export default function Textform(props) {  //prop means passsing propetyy
   }
   
   const [text, setText] = useState("");
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success");
+}
+
   
   return ( 
     <>
-    <div>
+    < div className="container"  style={{backgroundColor:props.mode==='dark'?'#042743':'white',color: props.mode==='dark'?'white':'#042743'}}>
       <h1>
         {props.heading} 
       </h1>   
       <div className="mb-3">        
         <label htmlFor="MyBox" className="form-label"></label>
-        <textarea className="form-control"value={text} onChange={handleOnChange} id="MyBox" rows="4"></textarea>
-        <button className="btn btn-primary mx-2" onClick={handleupClick} >Convert to upper case</button>   
-        <button className="btn btn-primary mx-2 " onClick={handletoClick} >Convert to lower case</button>    
+        <textarea className="form-control"value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="MyBox" rows="6"></textarea>
+        <button className="btn btn-primary mx-2 my-3" onClick={handleupClick} >Convert to upper case</button>   
+        <button className="btn btn-primary mx-2 my-3" onClick={handletoClick} >Convert to lower case</button>  
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>  
      
       </div>
     </div>
-    <div className="container my-3">
+    <div className="container my-3"   style={{color:props.mode==='dark'?'white':'black'}}>
 
       <h1>{text}</h1>
       <p>your text has {text.length}characters</p>   
       <p>And {text.split(" ").length} words</p>  
 
-      <p>Time taken to read words is {text.split(" ").length*0.008}minutes</p>
+      <p>Time taken to read words is  {text.split(" ").length*0.008}minutes</p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter text to preview it"}</p>
 
     </div>
     
